a8cd26f268aa328543cdd178b67e07180bd52914
nyambujoy <joyliznyambura@gmail.com>
2024-10-30 15:06:44 +0300

    finished the welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="display">
+        <p>The year is 2045, Disney lies in ruins, its magic lost. Abandoned and forgotten, noble princes and princesses are imprisoned their souls tormented by sinister forces. Only the bravest can venture into this cursed domain to play a haunting game of memory, to break the malevolent curse. Will you succeed in freeing the lost royals, or will you become another lost soul trapped in this forsaken kingdom.</p>
+
+        <h2>Play to save these Damned Souls</h2>
+        <div className="btns">
+          <button>Easy</button>
+          <button>Medium</button>
+          <button>Hard</button>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
   )
 }
 
